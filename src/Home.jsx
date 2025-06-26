1487d6b27a88077c697f4be5fa21b1660272f7e4
pasipasipada <irfanoputra27@gmail.com>
2025-06-26 15:57:45 +0700

    feat: add navigate

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router";
 
 export default function Home() {
+  const navigate = useNavigate();
+  
+  function handleClick() {
+    navigate({
+      pathname: "/data",
+      search: "?sort=asc",
+      hash: "#top",
+    });
+  }
+
   return (
     <div>
       <h1>Home</h1>
       <p>Welcome to the Home Page</p>
+      <button onClick={handleClick}>Go to Data Page</button>
     </div>
   );
 }
